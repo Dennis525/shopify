1117146deb80ff01366a985573642d866317a6de
Dennis Okumu <dennisokumu72@yahoo.com>
2022-06-12 23:34:07 +0300

    publish-and-build-an-app-to-Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import data from "./data.js";
+import path from 'path';
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
@@ -32,6 +32,12 @@ app.use("/api/products", productRouter);
 
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/front/build')))
+app.get('*', (req,res,next) => {
+  res.sendFile(path.join(__dirname, '/front/build/index.html'))
+})
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 
