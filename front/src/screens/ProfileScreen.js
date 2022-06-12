1c334ac176eecbc377fd9fcb15e8da68ac9dc58e
Dennis Okumu <dennisokumu72@yahoo.com>
2022-06-12 22:33:03 +0300

    added dot

--- front/src/screens/ProfileScreen.js
+++ front/src/screens/ProfileScreen.js
@@ -50,7 +50,7 @@ export default function ProfileScreen() {
         });
         ctxDispatch({ type: 'USER_SIGNIN', payload: data});
         localStorage.setItem('userInfo', JSON.stringify(data));
-        toast.success('User updated successfully');
+        toast.success('User updated. successfully');
     } catch (err) {
         dispatch({
             type: 'FETCH_FAIL',
