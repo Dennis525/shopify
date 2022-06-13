a6bc7c17d427bd34c51a2a2cfbbecb4d44b61da5
Dennis Okumu <dennisokumu72@yahoo.com>
2022-06-13 19:53:40 +0300

    Implementation of admin dashboard

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { isAuth } from "../utils.js";
+import { isAdmin, isAuth } from "../utils.js";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
+import User from '../models/userModel.js';
+import Product from '../models/productModel.js';
 
 const orderRouter = express.Router();
 orderRouter.post(
@@ -23,50 +25,91 @@ orderRouter.post(
     res.status(201).send({ message: "New Order Created", order });
   })
 );
-
 orderRouter.get(
-  '/mine', 
+  "/summary",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.aggregate([
+      {
+        $group: {
+          _id: null,
+          numOrders: { $sum: 1 },
+          totalSales: { $sum: "$totalPrice" },
+        },
+      },
+    ]);
+    const users = await User.aggregate([
+      {
+        $group: {
+          _id: null,
+          numUsers: { $sum: 1 },
+        },
+      },
+    ]);
+    const dailyOrders = await Order.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+          orders: { $sum: 1 },
+          sales: { $sum: "$totalPrice" },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+    const productCategories = await Product.aggregate([
+      {
+        $group: {
+          _id: "$category",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    res.send({ users, orders, dailyOrders, productCategories });
+  })
+);
+orderRouter.get(
+  "/mine",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const orders = await Order.find({user: req.user._id});
+    const orders = await Order.find({ user: req.user._id });
     res.send(orders);
   })
-)
+);
 orderRouter.get(
-    '/:id',
-    isAuth,
-    expressAsyncHandler(async (req, res) => {
-      const order = await Order.findById(req.params.id);
-      if (order) {
-        res.send(order);
-      } else {
-        res.status(404).send({ message: 'Order Not Found' });
-      }
-    })
-  );
+  "/:id",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
 
-  orderRouter.put(
-    '/:id/pay',
-    isAuth, 
-    expressAsyncHandler(async (req,res) => {
-        const order = await Order.findById(req.params.id);
-        if(order) {
-            order.isPaid = true;
-            order.paidAt = Date.now();
-            order.paymentResult = {
-                id: req.body.id,
-                status: req.body.status,
-                update_time: req.body.update_time,
-                email_address: req.body.email_address,
-            }
+orderRouter.put(
+  "/:id/pay",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
 
-            const updateOrder = await order.save();
-            res.send({ message: 'Order Paid', order: updateOrder});
-        }
-        else {
-            res.status(404).send({message: 'Order Not Found'});
-        }
-    })
-  )
+      const updateOrder = await order.save();
+      res.send({ message: "Order Paid", order: updateOrder });
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
 
 export default orderRouter;
--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 export const generateToken = (user) => {
   return jwt.sign(
     {
@@ -9,7 +9,7 @@ export const generateToken = (user) => {
     },
     process.env.JWT_SECRET,
     {
-      expiresIn: '30d',
+      expiresIn: "30d",
     }
   );
 };
@@ -20,13 +20,21 @@ export const isAuth = (req, res, next) => {
     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
-        res.status(401).send({ message: 'Invalid Token' });
+        res.status(401).send({ message: "Invalid Token" });
       } else {
         req.user = decode;
         next();
       }
     });
   } else {
-    res.status(401).send({ message: 'No Token' });
+    res.status(401).send({ message: "No Token" });
+  }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: "Invalid Admin Token" });
   }
 };
