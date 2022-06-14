2444077622330cce5c2c002f00073c4f658d08dc
Dennis Okumu <dennisokumu72@yahoo.com>
2022-06-14 16:31:51 +0300

    Deliver Orders

--- front/src/screens/OrderScreen.js
+++ front/src/screens/OrderScreen.js
@@ -14,6 +14,7 @@ import { getError } from "../utils";
 import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Button from "react-bootstrap/Button";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -31,6 +32,14 @@ function reducer(state, action) {
       return { ...state, loadingPay: false, errorPay: action.payload };
     case "PAY_RESET":
       return { ...state, loadingPay: false, successPay: false };
+    case "DELIVER_REQUEST":
+      return { ...state, loadingDeliver: true };
+    case "DELIVER_SUCCESS":
+      return { ...state, loadingDeliver: false, successDeliver: true };
+    case "DELIVER_FAIL":
+      return { ...state, loadingDeliver: false };
+    case "DELIVER_RESET":
+      return { ...state, loadingDeliver: false, successDeliver: false };
     default:
       return state;
   }
@@ -44,7 +53,15 @@ export default function OrderScreen() {
   const navigate = useNavigate();
 
   const [
-    { loading, error, order, successPay, loadingPay },
+    {
+      loading,
+      error,
+      order,
+      successPay,
+      loadingPay,
+      loadingDeliver,
+      successDeliver,
+    },
     dispatch,
   ] = useReducer(reducer, {
     loading: true,
@@ -108,10 +125,13 @@ export default function OrderScreen() {
     if (!userInfo) {
       return navigate("/login");
     }
-    if (!order._id || successPay || (order._id && order._id !== orderId)) {
+    if (!order._id || successPay || successDeliver || (order._id && order._id !== orderId)) {
       fetchOrder();
       if (successPay) {
         dispatch({ type: "PAY_RESET" });
+      }
+      if (successDeliver){
+        dispatch({type: "PAY_RESET"})
       }
     } else {
       const loadPayPalScript = async () => {
@@ -129,7 +149,24 @@ export default function OrderScreen() {
       };
       loadPayPalScript();
     }
-  }, [order, userInfo, orderId, navigate, paypalDispatch, successPay]);
+  }, [order, userInfo, orderId, navigate, paypalDispatch, successPay,successDeliver,]);
+  async function deliverOrderHandler() {
+    try {
+      dispatch({ type: 'DELIVER_REQUEST' });
+      const { data } = await axios.put(
+        `/api/orders/${order._id}/deliver`,
+        {},
+        {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({ type: 'DELIVER_SUCCESS', payload: data });
+      toast.success('Order is delivered');
+    } catch (err) {
+      toast.error(getError(err));
+      dispatch({ type: 'DELIVER_FAIL' });
+    }
+  }
   return loading ? (
     <LoadingBox></LoadingBox>
   ) : error ? (
@@ -188,7 +225,8 @@ export default function OrderScreen() {
                           src={item.image}
                           alt={item.name}
                           className="img-fluid rounded img-thumbnail"
-                        ></img>{""}
+                        ></img>
+                        {""}
                         <Link to={`/product/${item.slug}`}>{item.name}</Link>
                       </Col>
                       <Col md={3}>
@@ -247,6 +285,16 @@ export default function OrderScreen() {
                       ></PayPalButtons>
                     )}
                     {loadingPay && <LoadingBox></LoadingBox>}
+                  </ListGroup.Item>
+                )}
+                 {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                  <ListGroup.Item>
+                    {loadingDeliver && <LoadingBox></LoadingBox>}
+                    <div className="d-grid">
+                      <Button type="button" onClick={deliverOrderHandler}>
+                        Deliver Order
+                      </Button>
+                    </div>
                   </ListGroup.Item>
                 )}
               </ListGroup>
