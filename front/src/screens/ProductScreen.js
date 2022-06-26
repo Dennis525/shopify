a42723c093d6de3a853ff89d89bef7499bba09cb
Dennis Okumu <dennisokumu72@yahoo.com>
2022-06-26 14:19:29 +0300

    Adding multiple images of product

--- front/src/screens/ProductScreen.js
+++ front/src/screens/ProductScreen.js
@@ -44,6 +44,7 @@ function ProductScreen() {
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState('');
   const navigate = useNavigate();
+  const [selectedImage, setSelectedImage] = useState('');
   const params = useParams();
   const { slug } = params;
   const [{ loading, error, product, loadingCreateReview}, dispatch] = useReducer(reducer, {
@@ -124,7 +125,7 @@ function ProductScreen() {
         <Col md={6}>
           <img
             className="img-large"
-            src={product.image}
+            src={selectedImage || product.image}
             alt={product.name}
           ></img>
         </Col>
@@ -143,6 +144,24 @@ function ProductScreen() {
               ></Rating>
             </ListGroup.Item>
             <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              <Row xs={1} md={2} className="g-2">
+                {[product.image, ...product.images].map((x) => (
+                  <Col key={x}>
+                    <Card>
+                      <Button
+                        className="thumbnail"
+                        type="button"
+                        variant="light"
+                        onClick={() => setSelectedImage(x)}
+                      >
+                        <Card.Img variant="top" src={x} alt="product" />
+                      </Button>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </ListGroup.Item>
             <ListGroup.Item>
               Description:
               <p>{product.description}</p>
